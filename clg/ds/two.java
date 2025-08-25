c42aa4ecfaf839039b62e2afa85e2e896fd1c4d4
jeet solanki <solankijeet285@gmail.com>
2025-08-25 19:17:15 +0530

    initiall

---
+++ clg/ds/two.java
@@ -0,0 +1,7 @@
+package clg.ds;
+
+public class two {
+    public static void main(String[] args) {
+        System.out.println("ds-2");
+    }
+}
---
+++ clg/java/one.java
@@ -0,0 +1,7 @@
+package clg.java;
+
+public class one {
+    public static void main(String[] args) {
+        System.out.println("java");
+    }
+}
