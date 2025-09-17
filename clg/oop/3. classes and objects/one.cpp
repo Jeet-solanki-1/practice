f1943cd27c0f30357ffad17fda1327d27756b22e
jeet solanki <solankijeet285@gmail.com>
2025-09-17 20:50:56 +0530

    doing something

---
+++ clg/oop/3. classes and objects/one.cpp
@@ -0,0 +1,20 @@
+//3.1
+
+#include<iostream>
+using namespace std;
+
+class Main{
+	public:
+ void run();
+};
+
+void Main::run(){
+	cout<<"r"<<endl;
+}
+
+int main(){
+	Main main;
+	main.run();
+cout << "runed";
+return 0;
+}
